f1ecf74442041d02adbeda7ec1d68c7db22717f4
jopro275 <77482572+jopro275@users.noreply.github.com>
2022-11-17 20:35:33 -0500

    added a remove and sell button

---
+++ companeis.js
@@ -0,0 +1,50 @@
+//data - is an array of objects
+let companies = [
+	{
+	name: 'pfizer',
+	isUsComapny: true,
+	ticker: 'PFE',
+	price: 44.14,
+	amount: 0,
+	value: 0
+}, 
+{
+	name: 'merk',
+	isUsComapny: true,
+	ticker: 'PFE',
+	price: 94.36,
+	amount: 0,
+	value: 0
+}, 
+{
+	name: 'united healthcare',
+	isUsComapny: true,
+	ticker: 'PFE',
+	price: 515.15,
+	amount: 0,
+	value: 0
+},
+{
+	name: 'molina healthcare',
+	isUsComapny: true,
+	ticker: 'MOH',
+	price: 339.33,
+	amount: 0,
+	value: 0
+},
+{
+	name: 'avantor',
+	isUsComapny: true,
+	ticker: 'AVTR',
+	price: 20.21,
+	amount: 0,
+	value: 0
+}, 
+{
+	name: 'centene',
+	isUsComapny: true,
+	ticker: 'CNC',
+	price: 97.33,
+	amount: 0,
+	value: 0
+}];
--- script.js
+++ script.js
@@ -1,45 +1,116 @@
-let companies = [
-	{
-		ticker: "PFE",
-		amount: 0,
-		price: 44.14,
-		value: 0,
-	},
-	{
-		ticker: "MRK", 
-		amount: 0,
-		price: 94.36,
-		value: 0,
-	},
-	{
-		ticker: "UNH",
-		amount: 0,
-		price: 515.51,
-		value: 0,
-	},
-];
-
-let stockAmount;
-
 function generateTable() {
-    document.getElementById('amountPfe').innerHTML = companies[0].amount;
-	document.getElementById('amountMrk').innerHTML = companies[1].amount;
-    document.getElementById('amountUnh').innerHTML = companies[2].amount;
+	let tableBodyElement = document.getElementById('table-body');
+	tableBodyElement.innerHTML = null;
 
-	document.getElementById('valuePfe').innerHTML = `$${companies[0].price * companies[0].amount}`;
-    document.getElementById('valueMrk').innerHTML = `$${companies[1].price * companies[1].amount}`;
-    document.getElementById('valueUnh').innerHTML = `$${companies[2].price * companies[2].amount}`;
+	for (let i = 0; i < companies.length; i++) {
+		var company = companies[i];
+		var trElement = document.createElement('tr');
+
+		createAndAppendTd(trElement, company.name);
+		createAndAppendTd(trElement, company.isUsComapny ? 'Yes' : 'No');
+		createAndAppendTd(trElement, company.ticker.toUpperCase());
+		createAndAppendTd(trElement, `$${company.price}`);
+		createAndAppendTd(trElement, company.amount);
+		createAndAppendTd(trElement, `$${company.value}`);
+		
+		createAndAppendBtn(trElement, 'buy');
+		btn.addEventListener('click', function () {
+			buyStock(i);
+		});
+
+		createAndAppendBtn(trElement, 'sell', );
+		btn.addEventListener('click', function () {
+			sellStock(i);
+		});
+
+		createAndAppendBtn(trElement, 'remove', 'remove_btn');
+		btn.addEventListener('click', function () {
+			removeStock(i);
+		});
+
+		tableBodyElement.append(trElement);
+	}
+
+}
+
+function createAndAppendTd(trElement, innerText) {
+	let tdElement = document.createElement('td');
+	tdElement.innerText = innerText;
+	trElement.append(tdElement);
+}
+
+
+function createAndAppendBtn(trElement, innerText, className) {
+	btnTd = document.createElement('td');
+	btn = document.createElement('button');
+	btnTd.append(btn);
+	innerText = btn.innerText = innerText;
+	className = btn.classList.add(className);
+	trElement.append(btnTd);
 }
 
 function buyStock(index) {
-	stockAmount = prompt(`how much of ${companies[index].ticker} would you like to buy?`);
-    stockAmount = Number(stockAmount);
-	if(isNaN(stockAmount)){
-		alert('wrong input');
-		throw 'wrong input';
+	// Handle the button click
+	let amount = Number(
+		prompt(
+			`How many ${companies[index].name} stocks would you like to buy?`
+		)
+	);
+	if (isNaN(amount)) {
+		alert("Invalid amount. Please enter a valid amount.");
+	} else {
+		companies[index].amount += amount;
+		companies[index].value = companies[index].amount * companies[index].price;
+		generateTable();
 	}
-	alert(`your total is $${stockAmount * companies[index].price}`);
-    companies[index].amount += stockAmount;
-	generateTable();
-	
 }
+
+function sellStock(index) {
+	if( companies[index].amount <= 0){
+		alert('you dont have stock to sell');
+		throw 'invalid'
+	}
+	let amount = Number(
+		prompt(
+			`How many ${companies[index].name} stocks would you like to sell?`
+		)
+	);
+	if (isNaN(amount)) {
+		alert("Invalid amount. Please enter a valid amount.");
+		throw 'invalid'
+	}else{
+		companies[index].amount -= amount;
+		companies[index].value = companies[index].amount * companies[index].price;
+		generateTable();
+	}
+}
+
+function removeStock(index) {
+	companies.splice(index, 1);
+	console.log(companies.length);
+	generateTable();
+}
+
+function showAddCompanyForm() {
+	document.getElementById('addCompanyForm').style.display = 'block';
+}
+
+let formElement;
+function submitForm() {
+	formElement = document.getElementById('addCompanyForm');
+	let fd = new FormData(formElement);
+	let company = {};
+	company.name = fd.get('name');
+	company.isUsComapny = fd.get('isUsCompany') == 'on';
+	company.ticker = fd.get(`ticker`);
+	company.price = fd.get('price');
+	company.amount = 0;
+	company.value = 0;
+	companies.push(company);
+	generateTable();
+	formElement.reset();
+}
+
+let btnTd;
+let btn;
+generateTable();
